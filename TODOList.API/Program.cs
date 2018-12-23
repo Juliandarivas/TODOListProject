a260c07e7c6622a5659fa4b83e8d289f41597e5b
juliandarivas <juliandarivas@hotmail.com>
2018-12-22 21:28:53 -0500

    Initial commit

--- TODOList.API/Program.cs
+++ TODOList.API/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace TODO.API
+namespace TODOList.API
 {
     public class Program
     {
---
+++ TODOList.Entities/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace TODOList.Entities
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        
+    }
+}
